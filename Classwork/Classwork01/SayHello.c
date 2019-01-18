0a251e26c62a1c8a8acc2b365e7cd4f7eec85f77
Andrew Arteaga <aarteag4@lion.lmu.edu>
2019-01-17 20:53:58 -0800

    First Commit

--- Classwork/Classwork01/SayHello.c
+++ Classwork/Classwork01/SayHello.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
 //gcc SayHello.c && ./a.out
-int main() {
-    printf("Hello, World\n");
-    return 0;
+int main( int argc, char * argv[] ) {
+  char* input[100];
+  printf( "" );
+  char* nameInput = gets( input );
+  printf("Hello,", nameInput, "\n");
+  return 0;
 }
