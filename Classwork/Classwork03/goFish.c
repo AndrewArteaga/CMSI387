ed0466ac13c0999a9c8238ccbcaedb1af0064000
Andrew Arteaga <aarteag4@lion.lmu.edu>
2019-02-05 21:16:13 -0800

    Homework01 Started

---
+++ Classwork/Classwork03/goFish.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+//gcc SayHello.c && ./a.out
+int main( int argc, char * argv[] ) {
+int input = 0;    
+    while(input != -9999) {
+        input = scanf("%d", &input);
+}
